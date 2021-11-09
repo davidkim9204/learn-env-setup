da0b474d3e809582feeb01b72f0cfb47221656bd
David <davidkim9204@naver.com>
2021-11-09 22:46:08 +0900

    add loader

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,16 @@ module.exports = {
     output: {
         path: path.resolve('./dist'),
         filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader', //이렇게 처리된 js문자열 stylesheet 코드를 html style로 처리
+                    'css-loader' //css파일을 js 모듈 처럼 사용
+                ]
+            }
+        ]
     }
 }
